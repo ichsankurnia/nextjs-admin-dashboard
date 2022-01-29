37d6c275872553394f36fa470fd2f9ebb0cae44d
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-29 23:29:37 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        roboto: ["Roboto", "Helvetica", "Arial", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
