126dd9ff15f708e685f604a305ff0caad16d9b85
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-30 23:59:11 +0700

    disable warning recoil

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+
+const intercept = require("intercept-stdout")
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+
+// safely ignore recoil stdout warning messages 
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return ''
+  }
+  return text
 }
+
+// Intercept in dev and prod
+intercept(interceptStdout)
+
+module.exports = withPlugins([ [withImages] ], {
+  reactStrictMode: true,
+})
