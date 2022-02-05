bc9a09b6d424a895ed0a7fd735ab8139dbe949fc
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-02-05 20:47:58 +0700

    add loader when router is running

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,9 @@ import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { RecoilRoot } from 'recoil'
+import Loader from '../components/Loader'
+import { useEffect, useState } from 'react'
+import { Router } from 'next/router'
 
 type NextPageWithLayout = NextPage & {
   layout?: React.ElementType
@@ -43,6 +46,26 @@ interface ChildElement {
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const Layout = Component.layout || (({ children }: ChildElement) => <>{children}</>);
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
 
   return (
     <>
@@ -61,6 +84,11 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
+
+      {loading &&
+      <Loader />
+      }
+
       <RecoilRoot>
         <Layout>
           <Component {...pageProps} />
