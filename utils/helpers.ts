4aabcde655af14b20d647400c9fe893ffe501711
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-02-16 21:50:42 +0700

    add get set cookies

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -8,3 +8,28 @@ export function getTitleNav(routes: Array<any>, router: any) {
   });
   return name;
 }
+
+export function setCookie(cookieName: string, cookieValue: string, exdays: number, timestamp: number) {
+  const d = new Date();
+  // d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  // let expires = "expires="+ d.toUTCString();
+
+  d.setTime(timestamp)
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+export function getCookie(cookieName: string) {
+  let name = cookieName + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
