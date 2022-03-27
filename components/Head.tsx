60cb3417e3fcc4ed7a395dfa7075d6b0324e0a70
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-03-27 21:39:33 +0700

    update

--- components/Head.tsx
+++ components/Head.tsx
@@ -9,6 +9,15 @@ const Heading = ({ titlePage }: Props) => {
 	return (
 		<Head>
 			<title>{titlePage || 'Dashboard'} | AppName</title>
+			
+			<meta charSet="utf-8" />
+			<link rel="icon" href="/favicon.ico" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta name="theme-color" content="#333" />
+			<meta name="description" content="Admin dashboard using NextJS" />
+			<meta name='keywords' content='Admin dashboard using NextJS' />
+			<meta name='author' content='ichsankurnia' />
+			<link rel="apple-touch-icon" href="/favicon.ico" />
 		</Head>
 	)
 };
